6ed735bc803450d2d7c9786fedbcd5a22b1f2bd7
ibz-01 <ibrahimkhalid923@gmail.com>
2025-08-22 23:45:15 +0500

    Improved Collapse Button visibility and design

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,26 +1,32 @@
-
-
-
+import { useState } from "react";
 
 export default function Collapse(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div>
+    <div className="mb-3">
+      {/* Custom Collapse Button */}
       <button
-        className="btn btn-primary"
+        className="collapse-button"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target={`#${props.id}`}
-        aria-expanded="false"
+        aria-expanded={isOpen}
         aria-controls={props.id}
+        onClick={toggle}
       >
-        {props.title}
+        <span>{props.title}</span>
+        <span className={`collapse-arrow ${isOpen ? "open" : ""}`}>
+          <i className="bi bi-chevron-right"></i>
+        </span>
       </button>
 
+      {/* Collapsible Content */}
       <div style={{ minHeight: "120px" }}>
-        <div className="collapse collapse-horizontal" id={props.id}>
-          <div className="card card-body" style={{ width: "300px" }}>
-            {props.text}
-          </div>
+        <div className="collapse" id={props.id}>
+          <div className="collapse-content">{props.text}</div>
         </div>
       </div>
     </div>
